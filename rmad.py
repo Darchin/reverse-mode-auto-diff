d7f31c28a7b2fb0bc7de0eedc019a6fac676a3ac
Cinnamon <mix3d.r@gmail.com>
2024-04-15 20:44:20 +0330

    rewrote reverse pass to use toposort, now working as intended

--- rmad.py
+++ rmad.py
@@ -1,106 +1,120 @@
 import math
 from collections import deque
 
-class Variable:
-    tail = deque()
-    def __init__(self, value, local_gradients=[]):
+class Tensor:
+    def __init__(self, value, children=set()):
         self.value = value
-        self.local_gradients = local_gradients
         self.grad = 0
-        Variable.tail.append(self)
+        self.children: set = children
+        self._backward = lambda : None
     
+    def __str__(self):
+        return str(self.value)
+    def __repr__(self):
+        return self.__str__()
+    
+    class Primitives:
+        def add(var1, var2):
+            out = Tensor(value=var1.value + var2.value, children=set([var1, var2]))
+            def backward():
+                var1.grad = 1 * out.grad
+                var2.grad = 1 * out.grad
+            out._backward = backward
+            return out
+
+        def mul(var1, var2):
+            out = Tensor(value=var1.value * var2.value, children=set([var1, var2]))
+            def backward():
+                var1.grad = var2.value * out.grad
+                var2.grad = var1.value * out.grad
+            out._backward = backward
+            return out
+
+        def neg(var):
+            out = Tensor(value=-var.value, children=set([var]))
+            def backward():
+                var.grad = -1 * out.grad
+            out._backward = backward
+            return out
+
+        def inv(var):
+            out = Tensor(value=1/var.value, children=set([var]))
+            def backward():
+                var.grad = (-1 / (var.value**2)) * out.grad
+            out._backward = backward
+            return out
+
+        def pow(var1, var2):
+            out = Tensor(value=var1.value ** var2.value, children=set([var1, var2]))
+            def backward():
+                var1.grad = var2.value*var1.value**(var2.value-1) * out.grad
+                var2.grad = var1.value**var2.value*(math.log(var1.value)) * out.grad
+            out._backward = backward
+            return out
+
+        def sin(var):
+            out = Tensor(value=math.sin(var.value), children=set([var]))
+            def backward():
+                var.grad = math.cos(var.value) * out.grad
+            out._backward = backward
+            return out
+
+        def cos(var):
+            out = Tensor(value=math.cos(var.value), children=set([var]))
+            def backward():
+                var.grad = -math.sin(var.value) * out.grad
+            out._backward = backward
+            return out
+
+        def exp(var):
+            out = Tensor(value=math.exp(var.value), children=set([var]))
+            def backward():
+                var.grad = math.exp(var.value) * out.grad
+            out._backward = backward
+            return out
+
+        def log(var): # Base 'e'
+            out = Tensor(value=math.log(var.value), children=set([var]))
+            def backward():
+                var.grad = 1 / var.value * out.grad
+            out._backward = backward
+            return out
+
     def __add__(self, other):
-        return add(self, other)
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        return Tensor.Primitives.add(self, other)
     def __sub__(self, other):
-        return add(self, neg(other))
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        return Tensor.Primitives.add(self, Tensor.Primitives.neg(other))
     def __neg__(self):
-        return neg(self)
+        return Tensor.Primitives.neg(self)
     def __mul__(self, other):
-        return mul(self, other)
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        return Tensor.Primitives.mul(self, other)
     def __truediv__(self, other):
-        return mul(self, inv(other))
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        return Tensor.Primitives.mul(self, Tensor.Primitives.inv(other))
     def __pow__(self, other):
-        return pow(self, other)
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        return Tensor.Primitives.pow(self, other)
     
-def add(var1: Variable, var2: Variable) -> tuple:
-    value = var1.value + var2.value
-    local_gradients = [
-        (var1, 1),
-        (var2, 1)
-    ]
-    return Variable(value, local_gradients)
+def topoSort(root: Tensor) -> deque:
+    # Returns a deque with the terminal nodes (ie the inputs x_1,..., x_n) appended first.
+    sorted_deque = deque()
+    visited = set()
+    def ts(v: Tensor):
+        if v not in visited:
+            visited.add(v)
+        for c in v.children:
+          ts(c)
+        sorted_deque.append(v)
+    ts(root)
+    return sorted_deque
 
-def mul(var1: Variable, var2: Variable) -> tuple:
-    value = var1.value * var2.value
-    local_gradients = [
-        (var1, var2.value),
-        (var2, var1.value)
-    ]
-    return Variable(value, local_gradients)
-
-def neg(var: Variable) -> tuple:
-    value = -1 * var.value
-    local_gradients = [
-        (var, -1)
-    ]
-    return Variable(value, local_gradients)
-
-def inv(var: Variable) -> tuple:
-    value = 1 / var.value
-    local_gradients = [
-        (var, -1 / var.value**2)
-    ]
-    return Variable(value, local_gradients)
-
-def pow(var1: Variable, var2: Variable) -> tuple:
-    value = var1.value ** var2.value
-    local_gradients = [
-        (var1, var2.value*var1.value**(var2.value-1)),
-        (var2, var1.value**var2.value*(math.log(var1.value)))
-    ]
-    return Variable(value, local_gradients)
-
-def sin(var: Variable) -> tuple:
-    value = math.sin(var.value)
-    local_gradients = [
-        (var, math.cos(var.value))
-    ]
-    return Variable(value, local_gradients)
-
-def cos(var: Variable) -> tuple:
-    value = math.cos(var.value)
-    local_gradients = [
-        (var, -math.sin(var.value))
-    ]
-    return Variable(value, local_gradients)
-
-def exp(var: Variable) -> tuple:
-    value = math.exp(var.value)
-    local_gradients = [
-        (var, math.exp(var.value))
-    ]
-    return Variable(value, local_gradients)
-
-def log(var: Variable) -> tuple: # Base 'e'
-    value = math.log(var.value)
-    local_gradients = [
-        (var, 1/var.value)
-    ]
-    return Variable(value, local_gradients)
-
-def getGradients(func: Variable):
+def calcGradients(func: Tensor):
     "Returns the gradient of the function with respect to its inputs."
-    def reversePass(var: Variable):
-        for child_var, local_grad in var.local_gradients:
-            child_var.grad += var.grad * local_grad
-        if len(Variable.tail) != 0:
-            child_var = Variable.tail.pop()
-            reversePass(child_var)
-    # Depth first
-    # def reversePass(var: Variable, dvar: float):
-    #     for child_var, local_grad in var.local_gradients:
-    #         child_partial = dvar * local_grad
-    #         gradients[child_var] += child_partial
-    #         reversePass(child_var, child_partial)
+    stack = topoSort(func)
     func.grad = 1
-    reversePass(func)
+    while len(stack) != 0: 
+        tensor = stack.pop()
+        tensor._backward()
--- test.py
+++ test.py
@@ -1,16 +1,15 @@
 from rmad import *
 def main():
-    f = lambda x, y: exp(-(sin(x)-cos(y))**Variable(2))
-    x = Variable(1.0)
-    y = Variable(1.0)
+    f = lambda x, y: Tensor.Primitives.exp(-(Tensor.Primitives.sin(x)-Tensor.Primitives.cos(y))**2)
+    x = Tensor(1.0)
+    y = Tensor(1.0)
     z = f(x, y)
-    gradients = getGradients(z)
-
+    calcGradients(z)
     print("The partial derivative of z with respect to x =", x.grad)
     print("The partial derivative of z with respect to y =", y.grad)
 
     # finite differences
-    # h = 1e-8
+    # h = 1e-9
     # f = lambda x, y: math.exp(-(math.sin(x)-math.cos(y))**2)
     # dx = (f(1+h,1)-f(1,1))/h
     # dy = (f(1,1+h)-f(1,1))/h
